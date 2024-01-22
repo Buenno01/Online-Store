7f1004fb475ec78a48e90872b99f5cdbcb165a80
Vinicius Bueno Costa <buenov535@gmail.com>
2024-01-22 17:12:44 -0300

    feat: added states to be menaged

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Home from './pages/Home';
 import Layout from './components/Layout';
+import { ProductType } from './@types/ProductType';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [
+    searchedProducts,
+    setSearchedProducts,
+  ] = useState<ProductType[] | undefined >(undefined);
   return (
     <Routes>
-      <Route path="/" element={ <Layout /> }>
-        <Route index element={ <Home /> } />
+      <Route
+        path="/"
+        element={
+          <Layout
+            searchQuery={ searchQuery }
+            setSearchQuery={ setSearchQuery }
+            setSearchedProducts={ setSearchedProducts }
+          />
+      }
+      >
+        <Route index element={ <Home searchedProducts={ searchedProducts } /> } />
       </Route>
     </Routes>
   );
