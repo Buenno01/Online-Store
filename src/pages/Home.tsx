73423e67bdbec82b88eb9d0c9aaa7d20ed913b92
Vinicius Bueno Costa <buenov535@gmail.com>
2024-01-22 12:05:24 -0300

    feat: added aside with categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCategories } from '../services/api';
+import { CategoryType } from '../@types/CategoryType';
+import Aside from '../components/Aside';
 
 function Home() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = await getCategories();
+
+      setCategories(data);
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
-    <div data-testid="home-initial-message">
-      {
+    <main>
+      <Aside categories={ categories } />
+      <div data-testid="home-initial-message">
+        {
         products.length === 0
          && 'Digite algum termo de pesquisa ou escolha uma categoria.'
       }
-    </div>
+      </div>
+    </main>
   );
 }
 
