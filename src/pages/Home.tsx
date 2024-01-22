fde07010e18eade4861f3ed862a545dc7eee99d9
Vinicius Bueno Costa <buenov535@gmail.com>
2024-01-22 17:13:52 -0300

    feat: using the states to search for an item on the API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,14 @@ import { useEffect, useState } from 'react';
 import { getCategories } from '../services/api';
 import { CategoryType } from '../@types/CategoryType';
 import Aside from '../components/Aside';
+import ProductCard from '../components/ProductCard';
+import { ProductType } from '../@types/ProductType';
 
-function Home() {
-  const [products, setProducts] = useState([]);
+type HomeProps = {
+  searchedProducts: ProductType[] | undefined,
+};
+
+function Home({ searchedProducts }: HomeProps) {
   const [categories, setCategories] = useState<CategoryType[]>([]);
 
   useEffect(() => {
@@ -18,13 +23,21 @@ function Home() {
   }, []);
 
   return (
-    <main>
+    <main className="flex">
       <Aside categories={ categories } />
+      <section>
+        {
+        searchedProducts as ProductType[]
+        && searchedProducts?.map(
+          (product) => <ProductCard key={ product.id } product={ product } />,
+        )
+}
+      </section>
       <div data-testid="home-initial-message">
         {
-        products.length === 0
+        !searchedProducts
          && 'Digite algum termo de pesquisa ou escolha uma categoria.'
-      }
+        }
       </div>
     </main>
   );
