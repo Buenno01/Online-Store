da1dab356e459dd7a797f9df284eb08299d6fdd3
Vinicius Bueno Costa <buenov535@gmail.com>
2024-01-23 12:27:29 -0300

    feat: produtos podem ser adicionado ao carrinho e sao salvos no local storage

---
+++ src/pages/ShoppingCart.tsx
@@ -0,0 +1,50 @@
+import { Dispatch, SetStateAction } from 'react';
+import { ProductType } from '../@types/ProductType';
+import ProductCard from '../components/ProductCard';
+
+type ShoppingCartProps = {
+  products: ProductType[],
+  setProducts: Dispatch<SetStateAction<ProductType[]>>
+};
+
+interface ShoppingCartProduct extends ProductType {
+  quantity: number,
+}
+
+function ShoppingCart({ products, setProducts }: ShoppingCartProps) {
+  const filteredProducts = products.reduce((acc, current, index, arr) => {
+    const newArrayFilter = acc.filter((e) => e.id === current.id);
+    const quantity = arr.filter((e) => e.id === current.id).length;
+    const newItem: ShoppingCartProduct = current as ShoppingCartProduct;
+    newItem.quantity = quantity;
+
+    if (newArrayFilter.length === 0) {
+      return [...acc, newItem];
+    }
+
+    return acc;
+  }, [] as ShoppingCartProduct[]);
+  return (
+    <div className="shopping-cart-empty-page">
+      <h1>Carrinho de Compras</h1>
+
+      {products.length !== 0 ? (
+        filteredProducts.map((item) => (
+          <ProductCard
+            key={ item.id }
+            product={ item }
+            setShoppingCartItems={ setProducts }
+            shoppingCartItems={ products }
+            quantity={ item.quantity }
+          />
+        ))
+      ) : (
+        <div className="empty-message" data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ShoppingCart;
